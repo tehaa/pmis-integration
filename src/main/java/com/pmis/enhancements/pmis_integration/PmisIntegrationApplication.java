330bd045a9f4a298d39a7f9ef38f84e73665d9ef
ahmedabdelrhman <ahmed.abdelrahman@qeema.net>
2024-07-14 17:43:18 +0300

    project structure

---
+++ src/main/java/com/pmis/enhancements/pmis_integration/PmisIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.pmis.enhancements.pmis_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmisIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmisIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pmis/enhancements/pmis_integration/PmisIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmis.enhancements.pmis_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmisIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
